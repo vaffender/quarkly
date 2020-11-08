ac74436c3623c3b119a2780e9696930cef47d5b0
vaffender <71644213+vaffender@users.noreply.github.com>
2020-11-08 13:13:22 +0300

    Initial commit

---
+++ qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa7bfaa8996cc001edc7b41",
+				"5fa7bfaa8996cc001edc7b43"
+			]
+		},
+		"5fa7bfaa8996cc001edc7b41": {
+			"id": "5fa7bfaa8996cc001edc7b41",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa7bfaa8996cc001edc7b43": {
+			"id": "5fa7bfaa8996cc001edc7b43",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,292 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Box, Text, Button, Input } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section, StackItem, Stack, Formspree, SocialMedia } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Box
+			display="flex"
+			padding="12px 0"
+			justify-content="space-between"
+			align-items="center"
+			flex-direction="row"
+			md-flex-direction="column"
+			background="#ffffff"
+		>
+			<Section>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+					<Override slot="link-404">
+						Один
+					</Override>
+					<Override slot="link-index">
+						Два
+					</Override>
+				</Menu>
+			</Section>
+		</Box>
+		<Section padding="100px 0" sm-padding="40px 0">
+			<Override slot="SectionContent" align-items="center" />
+			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
+				Лендос
+			</Text>
+			<Text as="p" font="--lead" margin="20px 0 0 0">
+				Епта
+			</Text>
+			<Button font="--lead" margin="20px" transition="all 0s --transitionTimingFunction-easeOut 0s">
+				Жать сюда
+			</Button>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" color="--dark">
+			<Text as="h1" font="--headline1" margin="0 0 24px 0">
+				Кто?
+			</Text>
+			<Stack>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h3" font="--headline3" margin="12px 0">
+						Я?
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h3" font="--headline3" margin="12px 0">
+						Ну я!
+					</Text>
+				</StackItem>
+				<StackItem width="33.33%" md-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text as="h3" font="--headline3" margin="12px 0">
+						А что?
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="64px 0" sm-padding="40px 0" font="--base" color="--dark">
+			<Stack>
+				<StackItem width="50%" lg-width="100%" lg-order="2">
+					<Override slot="StackItemContent" text-align="center" />
+					<Stack>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=582&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Метросексуал
+							</Text>
+							<Text color="--grey" margin="0">
+								Гей
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1546672741-d327539d5f13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Негр
+							</Text>
+							<Text color="--grey" margin="0">
+								Раб
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1544005313-94ddf0286df2?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Александра
+							</Text>
+							<Text color="--grey" margin="0">
+								Маркетолог
+							</Text>
+						</StackItem>
+						<StackItem width="50%" sm-width="100%">
+							<Override slot="StackItemContent" background="--color-lightD1" padding="20px" flex-direction="column" />
+							<Box
+								width="170px"
+								height="170px"
+								margin="0 auto"
+								background="url(https://images.unsplash.com/photo-1510227272981-87123e259b17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=666&q=80) 50% 0/cover no-repeat"
+								border-radius="50%"
+							/>
+							<Text as="h3" font="--headline3" margin="20px 0 5px 0">
+								Феменистка
+							</Text>
+							<Text color="--grey" margin="0">
+								Угнетает спермобаков
+							</Text>
+						</StackItem>
+					</Stack>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" lg-margin-bottom="24px" flex-direction="column" />
+					<Text color="--grey" letter-spacing="1px" text-transform="uppercase" margin="0 0 10px 0">
+						Наша команда
+					</Text>
+					<Text as="h2" font="--headline2" md-font="--headline3" margin="0">
+						Самая пиздатая.
+						<br />
+						Выберите нас.
+						<br />
+						Пожалуйста.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-light" color="--dark" padding="64px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Text
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+						margin="0"
+					>
+						С нами епта
+					</Text>
+					<Text font="--headline2" max-width="500px" margin="10px 0 0 0">
+						КОНТАКТИРУЙ.{" "}
+						<br />
+						Через мыло.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Formspree>
+						<Stack gap="16px">
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Name
+								</Text>
+								<Input max-width="400px" width="100%" name="name" />
+							</StackItem>
+							<StackItem width="50%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Email
+								</Text>
+								<Input max-width="400px" width="100%" type="email" name="email" />
+							</StackItem>
+							<StackItem width="100%">
+								<Override slot="StackItemContent" flex-direction="column" />
+								<Text font="--base" margin="0 0 4px 0">
+									Message
+								</Text>
+								<Input as="textarea" rows="4" width="100%" name="message" />
+							</StackItem>
+							<StackItem width="100%">
+								<Button>
+									Send
+								</Button>
+							</StackItem>
+						</Stack>
+					</Formspree>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section padding="60px 0" sm-padding="40px 0">
+			<SocialMedia
+				facebook="https://www.facebook.com/quarklyapp/"
+				twitter="https://twitter.com/quarklyapp"
+				youtube="https://www.youtube.com/channel/UCK5bXs2L0bbSMQ82BQ3hIkw"
+				vkontakte="https://vk.com/quarklyapp"
+				telegram="https://t.me/joinchat/DqSYDhS0R9nMRvOtFbIxrQ"
+			>
+				<Override
+					slot="link"
+					border-radius="50%"
+					color="--light"
+					margin="0 8px"
+					background="--color-grey"
+					hover-background="--color-greyD1"
+				/>
+			</SocialMedia>
+		</Section>
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
+			>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+				<Override slot="link-404">
+					404
+				</Override>
+				<Override slot="link-index">
+					index
+				</Override>
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				ebiskonem@tut.by
+			</Link>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
